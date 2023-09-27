b98df00048d5ab004539b63adfe77445b1709b4e
Mohan <mohanvenkata123@gmail.com>
2023-09-28 01:02:04 +0530

    changed urls

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    // baseURL: 'https://tourism-backend-ce6w.onrender.com',
-    baseURL: 'http://localhost:4201',
+    baseURL: 'https://tourism-backend-ce6w.onrender.com',
+    // baseURL: 'http://localhost:4201',
     headers: {
         contentType: 'application/json',
         Accept: 'application/json',
-        'Access-Control-Allow-Origin': 'https//localhost:3000'
-        // 'Access-Control-Allow-Origin': 'https://pvpsit.onrender.com'
+        // 'Access-Control-Allow-Origin': 'https//localhost:3000'
+        'Access-Control-Allow-Origin': 'https://pvpsit.onrender.com'
         
     }
 })
